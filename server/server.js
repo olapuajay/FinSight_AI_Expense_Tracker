29c17d69ed63e3840c9ba7835392a446d5a210ee
Olapu Ajay <olapuajay@gmail.com>
2025-08-25 22:09:24 +0530

    completed database configuration with mongoose(mongodb)

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
 dotenv.config();
+connectDB();
 
 const app = express();
 app.use(express.json());
 
 
 
-const port = process.env.PORT || 8001;
-
 app.get("/", (req, res) => {
   res.send({ message: "Hello from server!" });
 });
 
+const port = process.env.PORT || 8001;
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
 });
