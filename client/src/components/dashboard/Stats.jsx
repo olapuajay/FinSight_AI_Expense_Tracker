684c86f5d67770299d2b6533f8841cebb0c8258e
Olapu Ajay <olapuajay@gmail.com>
2025-09-19 20:26:16 +0530

    redesigned stats section with respective icons for a better UX

--- client/src/components/dashboard/Stats.jsx
+++ client/src/components/dashboard/Stats.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDashboardSummary } from "../../redux/slices/dashboardSlice";
+import { ChartNoAxesCombined, HandCoins, Wallet, NotepadText, Logs } from "lucide-react";
 
 const Stats = ({ userId }) => {
   const dispatch = useDispatch();
@@ -20,7 +21,7 @@ const Stats = ({ userId }) => {
   if (loading) {
     return (
       <div className="p-4 text-center">
-        <p className="animate-pulse text-gray-500">Loading your stats...</p>
+        <p className="animate-pulse text-[#6B7280]">Loading your stats...</p>
       </div>
     );
   }
@@ -36,7 +37,7 @@ const Stats = ({ userId }) => {
   }
   if (!summary) {
     return (
-      <div className="p-4 text-center text-gray-500">
+      <div className="p-4 text-center text-[#6B7280]">
         No data available for this month.
       </div>
     );
@@ -45,85 +46,118 @@ const Stats = ({ userId }) => {
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-2 mb-4">
-        <div className="p-4 bg-white rounded-2xl shadow">
-          <h3 className="md:text-lg text-sm font-semibold">Monthly Spending</h3>
-          <p className="md:text-2xl text-lg font-bold text-red-600">
-            ₹{summary.monthlySpending.total}
-          </p>
-          <p className="md:text-sm text-xs text-gray-500">
-            Change: {summary.monthlySpending.change}%
-          </p>
-        </div>
-
-        <div className="p-4 bg-white rounded-2xl shadow">
-          <h3 className="md:text-lg text-sm font-semibold">Savings Rate</h3>
-          <p className="md:text-2xl text-lg font-bold text-green-600">
-            ₹{summary.savingsRate.amount} / ₹{summary.savingsRate.target}
-          </p>
-          <p className="text-xs text-gray-500 mt-1">
-            {summary.savingsRate.percent}%{" "}
-            {summary.savingsRate.percent >= 75
-              ? "On track"
-              : summary.savingsRate.percent >= 50
-              ? "Needs improvement"
-              : "At risk"}
-          </p>
-        </div>
-
-        <div className="p-4 bg-white rounded-2xl shadow">
-          <h3 className="md:text-lg text-sm font-semibold">Budget Remaining</h3>
-          <p className="md:text-2xl text-lg font-bold text-blue-600">
-            ₹{summary.budgetRemaining.total}
-          </p>
-          <div className="flex flex-row-reverse gap-2 w-2/3">
-            <p className="md:text-sm text-xs text-gray-500">
-              {(summary.budgetRemaining.percentRemaining)}%
+        <div className="p-4 bg-[#E5E7EB] rounded-2xl shadow flex justify-between items-center">
+          <div>
+            <h3 className="md:text-lg text-sm font-semibold text-[#111827]">Monthly Spending</h3>
+            <p className="md:text-2xl text-lg font-bold text-red-600">
+              ₹{summary.monthlySpending.total}
             </p>
+            <p className={`md:text-sm text-xs ${
+              Number(summary.monthlySpending.change) < 0
+                ? 'text-green-500'
+                : Number(summary.monthlySpending.change) > 0
+                  ? 'text-red-500'
+                  : 'text-[#6B7280]'
+            }`}>
+              {Number(summary.monthlySpending.change) > 0 && <>📈 {summary.monthlySpending.change}% <span className="text-[#6B7280]">vs last month</span></>}
+              {Number(summary.monthlySpending.change) < 0 && <>📉 {Math.abs(summary.monthlySpending.change)}% <span className="text-[#6B7280]">vs last month</span></>}
+              {Number(summary.monthlySpending.change) === 0 && <>➡️ 0% <span className="text-[#6B7280]">vs last month</span></>}
+            </p>
+          </div>
+          <div className="md:text-4xl text-2xl text-[#2563EB] p-1 bg-white border-2 rounded-lg">
+            <ChartNoAxesCombined />
+          </div>
+        </div>
 
-            <div className="w-full bg-gray-200 rounded-full h-3 mt-2">
-              <div
-                className={`h-3 rounded-full ${
-                  100 - summary.budgetRemaining.percentRemaining >= 100
-                    ? "bg-red-500"
-                    : 100 - summary.budgetRemaining.percentRemaining >= 80
-                    ? "bg-orange-500"
-                    : "bg-blue-500"
-                }`}
-                style={{
-                  width: `${Math.min(
-                    100 - summary.budgetRemaining.percentRemaining,
-                    100
-                  )}%`,
-                }}
-              />
+        <div className="p-4 bg-[#E5E7EB] rounded-2xl shadow flex justify-between items-center">
+          <div>
+            <h3 className="md:text-lg text-sm font-semibold text-[#111827]">Savings Rate</h3>
+            <p className="md:text-2xl text-lg font-bold text-green-600">
+              ₹{summary.savingsRate.amount} / ₹{summary.savingsRate.target}
+            </p>
+            <p className="text-xs md:text-sm text-[#6B7280] mt-1">
+              {summary.savingsRate.percent}%{" "}
+              {summary.savingsRate.percent >= 75
+                ? "On track"
+                : summary.savingsRate.percent >= 50
+                ? "Needs improvement"
+                : "At risk"}
+            </p>
+          </div>
+          <div className="text-4xl text-[#2563EB] p-1 bg-white border-2 rounded-lg">
+            <HandCoins />
+          </div>
+        </div>
+
+        <div className="p-4 bg-[#E5E7EB] rounded-2xl shadow flex justify-between items-center">
+          <div className="w-2/3">
+            <h3 className="md:text-lg text-sm font-semibold text-[#111827]">Budget Remaining</h3>
+            <p className="md:text-2xl text-lg font-bold text-blue-600">
+              ₹{summary.budgetRemaining.total}
+            </p>
+            <div className="flex flex-row-reverse gap-2">
+              <p className="md:text-sm text-xs text-[#6B7280]">
+                {(summary.budgetRemaining.percentRemaining)}%
+              </p>
+
+              <div className="w-full bg-white rounded-full h-3 mt-2">
+                <div
+                  className={`h-3 rounded-full ${
+                    100 - summary.budgetRemaining.percentRemaining >= 100
+                      ? "bg-red-500"
+                      : 100 - summary.budgetRemaining.percentRemaining >= 80
+                      ? "bg-orange-500"
+                      : "bg-blue-500"
+                  }`}
+                  style={{
+                    width: `${Math.min(
+                      100 - summary.budgetRemaining.percentRemaining,
+                      100
+                    )}%`,
+                  }}
+                />
+              </div>
             </div>
+          </div>
+          <div className="text-4xl text-[#2563EB] p-1 bg-white border-2 rounded-lg">
+            <Wallet />
           </div>
         </div>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-12 gap-2">
-        <div className="md:col-span-4 col-span-12 p-4 bg-white rounded-2xl shadow">
-          <h3 className="md:text-lg text-sm font-semibold">Budget Summary</h3>
-          <p className="md:text-sm text-xs text-green-600">
-            {summary.budgetSummary.underBudget} categories under budget
-          </p>
-          <p className="md:text-sm text-xs text-yellow-600">
-            {summary.budgetSummary.nearLimit} categories near the limit
-          </p>
-          <p className="md:text-sm text-xs text-red-600">
-            {summary.budgetSummary.overLimit} categories over the limit
-          </p>
+        <div className="md:col-span-4 col-span-12 p-4 bg-[#E5E7EB] rounded-2xl shadow flex justify-between items-center">
+          <div>
+            <h3 className="md:text-lg text-sm font-semibold text-[#111827]">Budget Summary</h3>
+            <p className="md:text-sm text-xs text-green-600">
+              {summary.budgetSummary.underBudget} categories under budget
+            </p>
+            <p className="md:text-sm text-xs text-yellow-600">
+              {summary.budgetSummary.nearLimit} categories near the limit
+            </p>
+            <p className="md:text-sm text-xs text-red-600">
+              {summary.budgetSummary.overLimit} categories over the limit
+            </p>
+          </div>
+          <div className="text-4xl text-[#2563EB] p-1 bg-white border-2 rounded-lg">
+            <NotepadText />
+          </div>
         </div>
 
-        <div className="md:col-span-8 col-span-12 p-4 bg-white rounded-2xl shadow">
-          <h4 className="text-md font-semibold">Category Breakdown</h4>
+        <div className="md:col-span-8 col-span-12 p-4 bg-[#E5E7EB] rounded-2xl shadow">
+          <div className="flex justify-between items-center">
+            <h4 className="text-md font-semibold text-[#111827]">Category Breakdown</h4>
+            <div className="text-4xl text-[#2563EB] p-1 bg-white border-2 rounded-lg">
+              <Logs />
+            </div>
+          </div>
           <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 mt-2">
             {Object.entries(summary.categoryBreakdown).map(([cat, data]) => {
               const percent = Math.round(data.percent);
               return (
                 <div key={cat} className="col-span-1">
-                  <p className="text-sm font-medium capitalize">{cat}</p>
-                  <div className="w-full bg-gray-200 rounded-full h-2">
+                  <p className="text-sm font-medium capitalize text-[#6B7280]">{cat}</p>
+                  <div className="w-full bg-white rounded-full h-2">
                     <div
                       className={`h-2 rounded-full ${
                         percent >= 100
